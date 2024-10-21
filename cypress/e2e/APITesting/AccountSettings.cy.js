1a587cfe6b6c60f44347f36108362c7581bd887d
AswanthMaroli <aswanthaswu07@gmail.com>
2024-10-21 17:49:51 +0530

    account settings added two tests invalid user , unauthorized user request

--- cypress/e2e/APITesting/AccountSettings.cy.js
+++ cypress/e2e/APITesting/AccountSettings.cy.js
@@ -72,6 +72,40 @@ describe('Api Testing in the eventzet Account Settings component', () => {
             });
         });
 
+        it('GetAccountDetails - Invalid UserID', () => {
+        
+            cy.request({
+                method: 'GET',
+                url: 'https://testservices.eventzet.com/api/AccountSettings/GetAccountDetails?UserID=999',
+                headers: {
+                   'Authorization': `Bearer ${Cypress.env('authToken')}`
+                },
+                failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(200);  // need to get 404 not found but geeting 200  response is null array
+                expect(response.duration).to.be.below(3000);
+                expect(response.body).to.be.an('array');
+               
+            });
+        });
+
+        it('GetAccountDetails - Missing Authorization', () => {
+        
+            cy.request({
+                method: 'GET',
+                url: `https://testservices.eventzet.com/api/AccountSettings/GetAccountDetails?UserID=${userID}`,
+                headers: {
+                   Authorization: ''
+                },
+                failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(401);
+                expect(response.duration).to.be.below(3000);
+                expect(response.body).to.be.empty;
+                // expect(response.body).to.have.property('errors', 'Unauthorized');
+            });
+        });
+
     });
 
 
